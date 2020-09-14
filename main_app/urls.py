4e57bbda21b81e173a886f3d3bd0ac3a98391df9
Alejandra Patino <ale@alejandras-mbp.home>
2020-09-14 16:27:42 -0700

    added user authentication functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path ('about/', views.about, name='about'),
     path('sightings/', views.SightingList.as_view(), name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Sighting
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create views below:
 def home(request):
@@ -11,3 +13,18 @@ def about(request):
 
 class SightingList(ListView):
     model = Sighting
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('index')
+    else:
+        error_message = 'Invalid sign up - try again'
+        
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
