45c92bfb9641d0d6c0a30fbd6662d6b99ce77509
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-09-14 16:04:21 -0700

    create detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path ('about/', views.about, name='about'),
     path('sightings/', views.SightingList.as_view(), name='index'),
+    path('sightings/<int:pk>/', views.SightingDetail.as_view(), name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Sighting
 
 # Create views below:
@@ -10,4 +10,7 @@ def about(request):
     return render(request, 'about.html')
 
 class SightingList(ListView):
+    model = Sighting
+
+class SightingDetail(DetailView):
     model = Sighting
