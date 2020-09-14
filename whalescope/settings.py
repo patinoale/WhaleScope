c6d88d8124e345997e0cbf03b0d6b3bb1606622e
ChenguK <ChenguKargbo@gmail.com>
2020-09-14 16:36:00 -0400

    first commit

--- whalescope/settings.py
+++ whalescope/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o#vcx)pw*t#5mm2)6&4(f&uyjxea&!)7+q_rcs#=q*eweg=x8j'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
