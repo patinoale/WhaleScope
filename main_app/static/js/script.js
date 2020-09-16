ffd26921c24847dc1a8717c58983e42fd19286cc
ChenguK <ChenguKargbo@gmail.com>
2020-09-15 21:16:44 -0400

    adding update to comments

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,16 +1,8 @@
-// $("#edit_link").change(function() {
-//     if ($("#edit_field").show()) {
-// } else {
-//         $("#edit_field").hide();
-//     };
-// });
-//     $("#edit_link").trigger("change");
-
 $(document).ready(function(){
     $(".edit_field").hide();
 
 $(".edit_button").click(function() {
-    $(this).show();
-    $(".edit_field").toggle();
+    const id =  $(this)['0'].id.split('_')[2]
+    $(`#edit_field_${id}`).toggle();
     });
 });
