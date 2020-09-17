d4ae4e3a971fff9e882a59d729eefb7474887e05
patinoale <58011328+patinoale@users.noreply.github.com>
2020-09-17 10:58:13 -0700

    Update post_spider.py

--- scrapy/postscrape/postscrape/spiders/post_spider.py
+++ scrapy/postscrape/postscrape/spiders/post_spider.py
@@ -5,14 +5,7 @@ class PostsSpider(scrapy.Spider):
     
     start_urls = [
         'https://blog.scrapinghub.com/page/1/',
-        # 'https://blog.scrapinghub.com/page/2/',
     ]
-
-    # def parse(self, response):
-    #     page = response.url.split('/')[-1]
-    #     filename = 'posts-%s.html' % page
-    #     with open(filename, 'wb') as f:
-    #         f.write(response.body)
 
     def parse(self, response):
         for post in response.css('div.post-item'):
@@ -20,10 +13,6 @@ class PostsSpider(scrapy.Spider):
                 'title': post.css('.post-header h2 a::text')[0].get(),
                 'date': post.css('.post-header a::text')[1].get(),
                 'author': post.css('.post-header a::text')[2].get(),
-                # 'title': post.css('.post-item h2 a::text')[0].get(),
-                # 'date': post.css('.post-item a::text')[2].get(),
-                # 'author': post.css('.post-item a::text')[3].get(),
-                # 'article': post.css('.post-content p::text')[0].get(),
             }
         next_page = response.css('a.next-posts-link::attr(href)').get()
         if next_page is not None:
